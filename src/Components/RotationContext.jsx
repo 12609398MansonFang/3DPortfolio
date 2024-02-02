836927e476955bc98a2abdd48077666198aa7366
Manson Fang Work <88397763+12609398MansonFang@users.noreply.github.com>
2024-02-02 17:43:47 +1100

    q

--- src/Components/RotationContext.jsx
+++ src/Components/RotationContext.jsx
@@ -20,10 +20,10 @@ export const RotationProvider = ({ children }) => {
 
 export const useRotation = () => {
     const context = useContext(RotationContext);
-
+    
     if (!context) {
         throw new Error('useRotation must be used within a RotationProvider');
     }
-
+    
     return context;
 };
